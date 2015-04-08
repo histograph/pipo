784f97a40f2dc8c7ab55e2f22a24ca1c14a359ee
mmmenno <menno@denengelse.nl>
2015-04-08 14:26:33 +0200

    added table csvfiles to sql

--- sql/pipo.sql
+++ sql/pipo.sql
@@ -3,22 +3,29 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Gegenereerd op: 08 apr 2015 om 13:46
+-- Gegenereerd op: 08 apr 2015 om 14:22
 -- Serverversie: 5.5.38
 -- PHP-versie: 5.6.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Databank: `erfgoedenlocatie_pipo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `csvfiles`
+--
+
+CREATE TABLE `csvfiles` (
+`id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `filepath` varchar(255) NOT NULL,
+  `uploaded` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -58,6 +65,12 @@ CREATE TABLE `fieldmappings` (
 --
 
 --
+-- Indexen voor tabel `csvfiles`
+--
+ALTER TABLE `csvfiles`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexen voor tabel `datasets`
 --
 ALTER TABLE `datasets`
@@ -74,10 +87,12 @@ ALTER TABLE `fieldmappings`
 --
 
 --
+-- AUTO_INCREMENT voor een tabel `csvfiles`
+--
+ALTER TABLE `csvfiles`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT voor een tabel `fieldmappings`
 --
 ALTER TABLE `fieldmappings`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
