6a73aa1bc2ef196c5b0531ed7e764655c8a65e14
mmmenno <menno@denengelse.nl>
2015-05-26 14:29:13 +0200

    oops!

--- src/Pipo/Mapper/Provider/DataSetControllerProvider.php
+++ src/Pipo/Mapper/Provider/DataSetControllerProvider.php
@@ -870,15 +870,15 @@ class DataSetControllerProvider implements ControllerProviderInterface
             }
 
             if(isset($pit['geometry'])){
-                if(json_decode($pit['geometry']) != null){ // soms 
-                    $pit['geometry'] = json_decode(stripslashes($pit['geometry']));
-                }
-
+                
+                $pit['geometry'] = json_decode(stripslashes($pit['geometry']));
+                
                 if($pit['geometry']==null || $pit['geometry']==""){ // some municipalities go without geometry, see Holysloot
                     unset($pit['geometry']);
                 }
             }
             
+
             if($pit['id']!=""){
                 $pits[] = json_encode($pit,JSON_UNESCAPED_SLASHES);
             }
